0115e1db1ddaa9e74d30d004c181cf6bb175f807
LeoPrince <meobius123@gmail.com>
2020-02-22 18:20:26 +0800

    add securify review

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleAPICall(req, res) })
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
