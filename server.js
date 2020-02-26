896cc370e5c7b82114843af3df3f8ab9b42a710e
LeoPrince <meobius123@gmail.com>
2020-02-26 15:12:57 +0800

    fix express error

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = knex({
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {})
+app.get('/', (req, res) => { res.send('it is working!') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
